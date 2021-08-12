543333e4bd81562269d9ed29329c637b6bcdac15
ttodinator <ttodinator@gmail.com>
2021-08-12 00:55:14 +0000

    ispravljen search

--- src/app/restaurants/restaurants-list/restaurants-list.component.ts
+++ src/app/restaurants/restaurants-list/restaurants-list.component.ts
@@ -13,6 +13,7 @@ export class RestaurantsListComponent implements OnInit {
   restaurantsInit:restaurant[]=[]
   restaurantsSearch:restaurant[]=[];
   searchCriteria:string='';
+  display: string;
 
 
   constructor(private restaurantService:RestaurantService) { }
@@ -25,21 +26,26 @@ export class RestaurantsListComponent implements OnInit {
   }
 
   search(event:any){
-    if(event.key=='Backspace'){
-      this.restaurantsInit=this.restaurants
-      this.searchCriteria= this.searchCriteria.substring(0, this.searchCriteria.length-1);
-      //console.log(this.searchCriteria);
-      this.restaurantsInit=this.restaurantsInit.filter(word=>word.name.startsWith(this.searchCriteria));
-    }else{
-      this.searchCriteria=this.searchCriteria+event.key;
-      //console.log(this.searchCriteria);
-    }
-
-    if(this.searchCriteria.length<1){
-      this.restaurantsInit=this.restaurants;
-    }else{
-      this.restaurantsInit=this.restaurantsInit.filter(word=>word.name.startsWith(this.searchCriteria));
-    }
+    var query=event.target.value
+    this.restaurantsInit.forEach(item=>{
+      //item.name.startsWith(query)?console.log('a'):this.display='none';
+      var shouldShow = item.name.toLowerCase().indexOf(query) > -1;
+      this.display = shouldShow ? 'block' : 'none';
+    })
+    // if(event.key=='Backspace'){
+    //   this.restaurantsInit=this.restaurants
+    //   this.searchCriteria= this.searchCriteria.substring(0, this.searchCriteria.length-1);
+    //   //console.log(this.searchCriteria);
+    //   this.restaurantsInit=this.restaurantsInit.filter(word=>word.name.startsWith(this.searchCriteria));
+    // }else{
+    //   this.searchCriteria=this.searchCriteria+event.key;
+    //   //console.log(this.searchCriteria);
+    // }
+    // if(this.searchCriteria.length<1){
+    //   this.restaurantsInit=this.restaurants;
+    // }else{
+    //   this.restaurantsInit=this.restaurantsInit.filter(word=>word.name.startsWith(this.searchCriteria));
+    // }
   }
 
 
