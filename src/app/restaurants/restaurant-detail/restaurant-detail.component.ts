2bf8550f18fdc4bdfca11f70f3438300fc4ecf6c
nadjas99 <stojancevicnadja@gmail.com>
2021-08-12 23:32:57 +0200

    slider za slike

--- src/app/restaurants/restaurant-detail/restaurant-detail.component.ts
+++ src/app/restaurants/restaurant-detail/restaurant-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { restaurant } from 'src/app/_models/restaurants';
@@ -12,13 +12,22 @@ import { restaurant } from 'src/app/_models/restaurants';
 export class RestaurantDetailComponent implements OnInit {
 
   restaurant:restaurant;
+  imagesList: string[]=['assets/images/a (2).jpg','assets/images/a (3).jpg','assets/images/a (4).jpg','assets/images/a (5).jpg','assets/images/a (6).jpg'];
 
   constructor(private route:ActivatedRoute) { }
+
+  imgSlide={
+    freeMode:true,
+    slidesPerView:1,
+    slidesOffsetBefore:11,
+  };
 
   ngOnInit() {
     this.route.data.subscribe(data=>{
       console.log(data);
       this.restaurant=data.restaurant;
+    
+      
     })
   }
 
