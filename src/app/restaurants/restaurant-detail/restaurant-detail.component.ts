f809ab88a8a0a91faf2a6bf02febb63158060fe0
nadjas99 <stojancevicnadja@gmail.com>
2021-08-15 20:40:06 +0200

    forma konacno radi, dodat TOAST za potvrdu rezervacije

--- src/app/restaurants/restaurant-detail/restaurant-detail.component.ts
+++ src/app/restaurants/restaurant-detail/restaurant-detail.component.ts
@@ -1,12 +1,12 @@
 import { Component, ComponentFactoryResolver, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, ToastController } from '@ionic/angular';
 import { take } from 'rxjs/operators';
 import { restaurant } from 'src/app/_models/restaurants';
 import { User } from 'src/app/_models/user';
 import { AccountService } from 'src/app/_services/account.service';
 import { RestaurantService } from 'src/app/_services/restaurant.service';
-
+import { toastController } from '@ionic/core';
 
 
 @Component({
@@ -29,7 +29,7 @@ export class RestaurantDetailComponent implements OnInit {
   date:Date=new Date();
   user:User;
 
-  constructor(private route:ActivatedRoute, private restaurantService:RestaurantService,private accountService:AccountService) {
+  constructor(private route:ActivatedRoute,public toastController: ToastController, private restaurantService:RestaurantService,private accountService:AccountService) {
     this.accountService.currentUser$.pipe(take(1)).subscribe(user=>{
       this.user=user;
     })
@@ -69,7 +69,7 @@ export class RestaurantDetailComponent implements OnInit {
   }
 
 
-  reserve(){
+  async reserve(){
 
     let x=this.restaurant.restaurantId
     console.log(x);
@@ -79,10 +79,28 @@ export class RestaurantDetailComponent implements OnInit {
       seating:this.seating,
       date:this.date,
     });
+
+    const toastTrue=await this.toastController.create({
+      message: "Vasa rezervacija je napravljena",
+      duration: 5000
+
+    });
+    const toastFalse=await this.toastController.create({
+      message: "Za izabrane kriterijume nema slobodnih stolova",
+      duration: 5000
+
+    });
     console.log(this.model);
     this.restaurantService.reserve(this.model).subscribe(res=>{
       console.log(res);
+      
+        toastTrue.present();
+      
+    }, error=>{
+      toastFalse.present();
     })
+    
+    
 
   }
 
